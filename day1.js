a817c4297a8cb5451c88f077f302745b3b3e7e72
Antonio <dontony@mit.edu>
2013-06-10 13:22:34 -0400

    added tokenized, cleaned up js

--- day1.js
+++ day1.js
@@ -1,8 +1,26 @@
-        for(var i = 0; i < 10; i++){
-            $("#table1").add(i);
-        }
-        function calculate(){
-            var input = $("#input:first").val();
-            $("#text1_out:first").text(input);
+
+        function calculate(string){
             console.log("click");
+            return tokenize(string);
         }
+function tokenize(string){
+    var regex = /\d+|[\+\/\*\-\(\)]/g;
+    var tokens = string.match(regex);
+    return JSON.stringify(tokens);
+    //g is a flag, means global match
+}
+function setup_calc(calc){
+    var input=$("<input></input>", {type:"text", size:"50"});
+    var output=$("<div></div>");
+    var button=$("<button> Calculate </button>");
+    
+    button.bind("click", function(){
+        output.text(String(calculate(input.val())));
+    });
+    $(calc).append(input, button, output);
+}
+$(document).ready(function(){
+   $(".calculator").each(function(){
+      setup_calc(this);
+   });
+});

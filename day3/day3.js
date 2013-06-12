ce2371b439577d300a27c597569e77af3a88a70a
Antonio <dontony@mit.edu>
2013-06-12 09:42:54 -0400

    day3 stuff

---
+++ day3/day3.js
@@ -0,0 +1,18 @@
+function test_clear(){
+    var canvasJQ=$("#test_canvas");
+    var canvasDOM=canvasJQ[0];
+    var context=canvasDOM.getContext('2d');
+    //(x,y,dx, dy)
+    context.clearRect(0,0,canvasJQ.width(), canvasJQ.height());
+}
+function test_line(){
+    var canvasJQ=$("#test_canvas");
+    var canvasDOM=canvasJQ[0];
+    var context=canvasDOM.getContext('2d');
+    
+    context.beginPath();
+    context.moveTo(50,50);
+    context.lineTo(150,150);
+    context.arcTo(50,50);
+    context.stroke();
+}

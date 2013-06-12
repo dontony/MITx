594036598f7975e89441c05900ce15971682e607
Antonio <dontony@mit.edu>
2013-06-12 11:21:55 -0400

    added calculator.js, played around with canvas more

--- day3/day3.js
+++ day3/day3.js
@@ -52,6 +52,7 @@ function test_star(){
     context.lineJoin=("round");
     context.beginPath();
     context.moveTo(100, 50);
+    
 }
 function test_smile(){
     var canvasJQ=$("#test_canvas");
@@ -75,5 +76,51 @@ function test_smile(){
     context.fill();
     context.arc(130,70,10, 0, 2*Math.PI);
     context.fill();
+}
+function test_text(){
+    var canvasJQ=$("#test_canvas");
+    var canvasDOM=canvasJQ[0];
+    var context=canvasDOM.getContext('2d');
+    context.lineWidth=(10);
+    context.fillStyle='black';
+    context.lineCap='round';
+    context.lineJoin=("round");
+    context.font="20px Verdana";
+    context.textAlign="center";//left, right
+    //context.textBaseline="middle";//top, bottom, alphabetic
+    var text="56.5+37/2";
+    context.fillText(text+"="+calculator.calculate(text), 100, 190);
+}
+function test_mouse(){
+    var canvasJQ=$("#test_canvas");
+    var canvasDOM=canvasJQ[0];
+    var context=canvasDOM.getContext('2d');
+    context.lineWidth=(1);
+    context.strokeStyle='black';
+    context.lineCap='round';
+    context.lineJoin=("round");
+    context.font="20px Verdana";
+    context.textAlign="center";//left, right
+    var bg_image=$("<canvas width="+canvasJQ.width() +" height="+canvasJQ.height()+"></canvas>")[0];
+    var bg_ctx=bg_image.getContext('2d');
+    bg_ctx.fillStyle="lightgreen";
+    bg_ctx.fillRect(0,0,bg_image.width, bg_image.height);
     
+    canvasJQ.on("mousemove", function(event){
+        var mx = Math.round(event.pageX-canvasJQ.offset().left);
+        var my = Math.round(event.pageY-canvasJQ.offset().top);
+        //test_clear();
+        context.drawImage(bg_image, 0,0);
+        context.beginPath();
+        context.moveTo(mx-8, my-8);
+        context.lineTo(mx-3, my-3);
+        context.moveTo(mx-8, my+8);
+        context.lineTo(mx-3, my+3);
+        context.moveTo(mx+8, my+8);
+        context.lineTo(mx+3, my+3);
+        context.moveTo(mx+8, my-8);
+        context.lineTo(mx+3, my-3);
+        context.stroke();
+        
+    });
 }
